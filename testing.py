de778b9fcf76a25ddc64a57e6764d9a9e0e187f2
FawziLinggo <fawzilinggo@gmail.com>
2022-09-24 07:14:34 +0700

    Testing file

--- testing.py
+++ testing.py
@@ -1,8 +1,18 @@
-test = 'https://megapolitan.ayu.com/read/2022/09/18/11494351/2-stasiun-pengisian-kendaraan-listrik-umum-akan-dibangun-di-terminal'
+import requests
+from bs4 import BeautifulSoup
 
-check_link = test.split('.')
-if "kompas" in check_link:
-    print("cool")
-    print(test)
-else:
-    print("err")
+# test = 'https://megapolitan.ayu.com/read/2022/09/18/11494351/2-stasiun-pengisian-kendaraan-listrik-umum-akan-dibangun-di-terminal'
+#
+# check_link = test.split('.')
+# if "kompas" in check_link:
+#     print("cool")
+#     print(test)
+# else:
+#     print("err")
+
+
+kompas_domain_com = requests.get(
+    'https://www.kompas.com/edu/read/2022/09/22/114238371/ruu-sisdiknas-tak-masuk-prolegnas-nadiem-yang-penting-hati-tulus-kinerja')
+beautify = BeautifulSoup(kompas_domain_com.content, "html.parser")
+# print(beautify.find('div', {'class', 'read__time'}).contents[1].text)
+print(beautify.find('meta',attrs={'name':'content_PublishedDate'}).get('content'))
